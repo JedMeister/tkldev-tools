ed95783dac1ecc8e72e7ab651207e705b48826d6
Jeremy Davis <jeremy@turnkeylinux.org>
2018-07-25 14:50:07 +1000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 #!/bin/bash -e
 
-SRC_DIR=$PWD
-INS_DIR=/usr/local/bin
+# Bash installer for tkldev-tools
 
-for file in $(find $SRC_DIR/bin -type f); do 
-    ln -s $file $INS_DIR/$(basename $file)
+. lib/bash-common
+
+check_root
+
+SRC_PATH=$PWD
+BASE_PATH=/usr/local
+BIN_PATH=${BASE_PATH}/bin
+LIB_PATH=${BASE_PATH}/lib
+ETC_PATH=/etc/tkldev-tools
+CONF=${ETC_PATH}/conf
+CONF_EX=${SRC_PATH}/etc/conf.example
+
+for file in $(find ${SRC_PATH}/bin -executable -type f); do
+    ln -s ${file} ${BIN_PATH}/$(basename ${file})
 done
+for file in $(find ${SRC_PATH}/lib -type f); do
+    ln -s ${file} ${LIB_PATH}/$(basename ${file})
+done
+mkdir -p ${ETC_PATH}
+if [[ ! -f ${CONF} ]]; then
+    cp ${CONF_EX} ${CONF}
+else
+    grep LIB_PATH ${CONF} >/dev/null || warning "LIB_PATH not set in conf"
+    warning "${CONF} already exists, please check that it is compatible with ${CONF_EX}"
+fi
